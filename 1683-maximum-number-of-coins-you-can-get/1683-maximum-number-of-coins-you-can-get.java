aef5871eabe5a3bd25c0fa4854e3b93c22117491
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2025-08-24 09:07:28 +0530

    Time: 26 ms (96.51%), Space: 56.5 MB (55.29%) - LeetHub

---
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);  
+        int n = piles.length;
+        int sum = 0;
+        for (int i = n / 3; i < n; i += 2) {
+            sum += piles[i];
+        }
+
+        return sum;
+    }
+}
