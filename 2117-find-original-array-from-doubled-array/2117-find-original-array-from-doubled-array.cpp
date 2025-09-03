0bd65054b02fa5870a82c8d74a12fd1c88ac8ab9
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2025-09-03 10:31:30 +0530

    Time: 255 ms (12.79%), Space: 148.6 MB (27.67%) - LeetHub

---
+++ 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        int n = changed.size();
+        if(n%2!=0 ) return {};
+        map<int,int>freq;
+        for(int x : changed) freq[x]++;
+        sort(changed.begin(),changed.end());
+        vector<int>res;
+        for(int i=0;i<n;i++)
+        {
+            if(freq[changed[i]]==0) continue;
+            if(freq[2*changed[i]]==0) return {};
+            res.push_back(changed[i]);
+            freq[changed[i]]--;
+            freq[2*changed[i]]--;
+        }
+        return res;
+    }
+};
