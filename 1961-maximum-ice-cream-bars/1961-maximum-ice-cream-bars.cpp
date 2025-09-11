5aba27ec9babfc23d622ecdf2852838af0caa98d
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2025-09-11 23:48:39 +0530

    Time: 29 ms (77.31%), Space: 80.2 MB (61.38%) - LeetHub

---
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+    sort(costs.begin(), costs.end());
+    int sum = 0, cnt = 0;
+
+    for (int i = 0; i < costs.size(); i++) {
+        if (sum + costs[i] <= coins) 
+        {
+            sum += costs[i];
+            cnt++;
+        } else {
+            break; 
+        }
+    }
+    return cnt;
+    }
+};
