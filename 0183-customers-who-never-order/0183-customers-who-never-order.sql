c5d781f80f5f2174ff9895d6880b530be070325b
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-02-14 10:04:50 +0530

    Time: 524 ms (90.46%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,7 +1,5 @@
-/* Write your PL/SQL query statement below */
-SELECT c.name as CUSTOMERS FROM
-customers c
-LEFT JOIN
-orders o
-ON c.id=o.customerId
-WHERE o.id is NULL;
+--SCMR Subquery --> Single Column Multiple Row
+-- SELECT customerId from orders
+SELECT name AS customers from customers
+where id not in
+(SELECT customerid from orders);
