edc583cec3e2802ef3eff34dec8908ec0389dbdf
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-03-03 17:26:26 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.71%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        int l = 0;
+        int r = a.size() - 1;
+        
+        while(l <= r) {
+            int m = l + (r - l) / 2; 
+            
+            if(a[m] == target) {
+                return m;
+            }
+            else if(a[m] < target) {
+                l = m + 1;
+            }
+            else {
+                r = m - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
