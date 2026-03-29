bd6f86f431ea3f4940c192471fb76c61b73c3d9b
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-03-29 14:47:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (32.9%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        
+        // group 1: indices 0 and 2
+        string a1 = "";
+        a1 += s1[0];
+        a1 += s1[2];
+        
+        string a2 = "";
+        a2 += s2[0];
+        a2 += s2[2];
+        
+        // group 2: indices 1 and 3
+        string b1 = "";
+        b1 += s1[1];
+        b1 += s1[3];
+        
+        string b2 = "";
+        b2 += s2[1];
+        b2 += s2[3];
+        
+        sort(a1.begin(), a1.end());
+        sort(a2.begin(), a2.end());
+        sort(b1.begin(), b1.end());
+        sort(b2.begin(), b2.end());
+        
+        return (a1 == a2 && b1 == b2);
+    }
+};
