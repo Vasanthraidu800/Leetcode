18acfb7f4e38fdd53c5a412dd0c6ad5a4ed9f429
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-04-06 22:20:13 +0530

    Time: 27 ms (53.33%), Space: 37.8 MB (66.4%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        // Directions: North, East, South, West
+        vector<pair<int,int>> dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int d = 0; // starting facing North
+        
+        int x = 0, y = 0;
+        int maxDist = 0;
+
+        set<pair<int,int>> obs;
+        for(auto &o : obstacles) {
+            obs.insert({o[0], o[1]});
+        }
+
+        for(int cmd : commands) {
+            if(cmd == -1) {
+                d = (d + 1) % 4; // turn right
+            }
+            else if(cmd == -2) {
+                d = (d + 3) % 4; // turn left
+            }
+            else {
+            
+                for(int i = 0; i < cmd; i++) {
+                    int nx = x + dir[d].first;
+                    int ny = y + dir[d].second;
+
+                    // stop if obstacle
+                    if(obs.count({nx, ny})) break;
+
+                    x = nx;
+                    y = ny;
+
+                    maxDist = max(maxDist, x*x + y*y);
+                }
+            }
+        }
+
+        return maxDist;
+    }
+};
