831fa6669f2a94b09e0d4ab155d3a45d96df1334
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-04-10 22:38:53 +0530

    Time: 4 ms (52.52%), Space: 29.7 MB (47.69%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, vector<int>> mp;
+    
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]].push_back(i);
+        }
+        
+        int ans = INT_MAX;
+        
+
+        for (auto &p : mp) {
+            auto &v = p.second;
+        
+            if (v.size() < 3) continue;
+            
+            for (int i = 2; i < v.size(); i++) {
+                int dist = 2 * (v[i] - v[i - 2]);
+                ans = min(ans, dist);
+            }
+        }
+        
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
