e73bce8c480dfa814240472f0eaa6895d565bcdc
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-06-28 21:43:04 +0530

    Time: 4 ms (7.77%), Space: 12.7 MB (24.02%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(int idx, vector<int>& nums, vector<int>& temp) {
+        if (idx == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        solve(idx + 1, nums, temp);
+
+
+        temp.push_back(nums[idx]);
+        solve(idx + 1, nums, temp);
+
+
+        temp.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        solve(0, nums, temp);
+        return ans;
+    }
+};
