28d6342d46cb0b079cf719bf94ec939c449bfe11
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-07-16 15:42:19 +0530

    Time: 8 ms (74.42%), Space: 89.8 MB (82.11%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& org, int m, int n) {
-        if(m*n !=org.size()) return {};
+        if(m*n !=org.size()) return {}; 
         int k = org.size(); 
         vector<vector<int>>res(m,vector<int>(n));
         k=0;
