039d27f4840bc6498578f58ec6e1cdf5fb64eebb
Vasanth _Raidu <vasanthraiduposina@gmail.com>
2026-09-10 22:48:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (100%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,26 @@
+bool isPalindrome(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right) {
+        // Move left forward if not alphanumeric
+        while (left < right && !isalnum(s[left])) {
+            left++;
+        }
+
+        // Move right backward if not alphanumeric
+        while (left < right && !isalnum(s[right])) {
+            right--;
+        }
+
+        // Compare characters (case-insensitive)
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
